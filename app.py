db2e58c463c20165c5e4e5f036c521e7161c5433
ofvmkt <ofvmkt.monday@gmail.com>
2021-12-02 20:29:37 +0700

    install test

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def hello():
     return 'Web App with Python Flask!'
 
-app.run()
+if __name__ == '__main__':
+    app.run()
